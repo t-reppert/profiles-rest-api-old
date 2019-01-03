3f737ade43790239df663518f25aa6ecc1bf818c
Tom Reppert <tom.reppert@gmail.com>
2019-01-03 17:26:52 -0500

    Fixed bug in function creation.

--- src/profiles_project/profiles_api/serializers.py
+++ src/profiles_project/profiles_api/serializers.py
@@ -17,18 +17,18 @@ class UserProfileSerializer(serializers.ModelSerializer):
         fields = ('id', 'email', 'name', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
-        def create(self, validated_data):
-            """Create and return a new user."""
+    def create(self, validated_data):
+        """Create and return a new user."""
 
-            user = models.UserProfile(
-                email = validated_data['email'],
-                name = validated_data['name']
-            )
+        user = models.UserProfile(
+            email = validated_data['email'],
+            name = validated_data['name']
+        )
 
-            user.set_password(validated_data['password'])
-            user.save()
+        user.set_password(validated_data['password'])
+        user.save()
 
-            return user
+        return user
 
 
 class ProfileFeedItemSerializer(serializers.ModelSerializer):
